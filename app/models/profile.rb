706819254f39fd4f2cf981d94c8ab8a6b7d182bf
Iverick <iverick091@gmail.com>
2022-06-08 10:51:19 +0300

    Modified Profile method to return proper query value.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,9 +4,16 @@ class Profile < ApplicationRecord
   enum gender: [:male, :female]
 
   def self.profiles_older_18
-    ActiveRecord::Base.connection.execute(
+    @result = ActiveRecord::Base.connection.execute(
       "SELECT COUNT(*) 
       FROM profiles 
       WHERE birthday < current_date - interval '18 year'")
+    # Transforming query result value from hash to string
+    @count_entries = @result[0]
+    if @count_entries.include?('count')
+      return @count_entries['count']
+    else
+      return @count_entries
+    end
   end
 end
