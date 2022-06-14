bc41083440c03dbab1bbd5e1c1aee16e00868cab
Iverick <iverick091@gmail.com>
2022-06-14 22:11:39 +0300

    Added a unique name for user_id constraint for Profile model.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user, counter_cache: true
 
+  validates_uniqueness_of :name, scope: :user_id
+
   enum gender: [:male, :female]
 
   scope :adults, -> { where('birthday <= ?', 18.years.ago) }
